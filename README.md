8a98f5b39219eb3bbb469dec9c28242c2abb8d78
Aleksei Melnik <laleksss@gmail.com>
2023-07-07 18:25:43 +0300

    First steps of quality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # psa_dispay_configurator
 PSA matrix display configurator
+
+### Installation
+
+>python -m pip install virtualenv
+***
+>.\venv\Scripts\activate
+***
+Выполнить установку requirements.txt
--- main.py
+++ main.py
@@ -6,11 +6,6 @@ import threading
 from time import sleep
 from random import randint
 import os
-import PySide2
-
-dirname = os.path.dirname(PySide2.__file__)
-plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
 
 
 app = QtWidgets.QApplication([])
