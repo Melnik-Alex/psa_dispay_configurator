a426049fae8b9e2afdda59e358ba610c25501ea5
Aleksei Melnik <laleksss@gmail.com>
2023-07-13 22:15:43 +0300

    Created first version of Sender in sender.py

---
+++ Data/sender.py
@@ -0,0 +1,48 @@
+# importing the requests library
+import requests
+import uuid
+import hashlib
+import json
+
+# defining the api-endpoint
+API_ENDPOINT = "http://62.113.105.77:1234"
+
+# your API key here
+API_KEY = "dBzQDgaML14XfPamrPcXQA"
+USER_ID = str(uuid.getnode())
+
+
+class Sender():
+
+    def send_data(self, user_id, z0200, z0400, z0500, z0600, z2100):
+        data = {
+            "userId": USER_ID,
+            "username": '12345',
+            "zone0200": z0200,
+            "zone0400": z0400,
+            "zone0500": z0500,
+            "zone0600": z0600,
+            "zone2100": z2100,
+            "zone0f": "",
+            "zoneFF": ""
+        }
+        hash_data = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
+        # sending post request and saving response as response object
+        r = requests.post(url=API_ENDPOINT, json=data)
+
+        # extracting response text
+        response = r.text
+        assert hash_data == response, 'Контрольные суммы не совпадают!'
+        print('Recived hash: ', hash_data)
+        print('Calculated hash: ', response)
+        return response
+
+
+if __name__ == "__main__":
+    sender = Sender()
+    sender.send_data(user_id='laleksss',
+                     z0200='qwerнt',
+                     z0400='asdfg',
+                     z0500='zxcvb',
+                     z0600='hjkkjh',
+                     z2100='09weuosndls')
--- test.py
+++ test.py
@@ -1,5 +1,50 @@
+from flask import Flask, request
+from gevent.pywsgi import WSGIServer
+import os, json, datetime, hashlib
 
-a1 = ' '
+app = Flask(__name__)
 
-a2 = (float(len(a1))/2).is_integer()
-print(a2)
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    ctime = datetime.datetime.now()
+    if request.method == 'POST':
+        try:
+            if request.json['userId'] is not None:
+                data = request.json
+                try:
+                    try:
+                        os.mkdir('post_data/' + data['userId'], 0o755)
+                    except:
+                        pass
+                    try:
+                        os.mkdir('post_data/' + data['userId'] + '/' + str(ctime.year), 0o755)
+                    except:
+                        pass
+                    try:
+                        os.mkdir('post_data/' + data['userId'] + '/' + str(ctime.year) + '/' + str(ctime.month), 0o755)
+                    except:
+                        pass
+                    try:
+                        os.mkdir(
+                            'post_data/' + data['userId'] + '/' + str(ctime.year) + '/' + str(ctime.month) + '/' + str(
+                                ctime.day), 0o755)
+                    except:
+                        pass
+                except OSError:
+                    pass
+
+                with open('post_data/'+ data['userId'] + '/' + str(ctime.year) + '/' + str(ctime.month) + '/' + str(ctime.day) + '/' +data['userId']+'_data.json', 'w') as f:
+                    json.dump(request.json, f, indent=4)
+                response = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
+                return response
+            else:
+                return 'Use valid data'
+        except:
+            return 'Data is corrupted!'
+    else:
+        return 'Use POST requests'
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('', 1234), app)
+    http_server.serve_forever()
